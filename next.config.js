9b49a7f5ef4083b86a446b80f470c410fd93bb55
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2024-02-19 02:49:12 +0100

    Code refactoring

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: '/about',
-                destination: `${process.env.BACKEND_URL}/about/`,
-            },
-            {
-                source: '/projects',
-                destination: `${process.env.BACKEND_URL}/projects/`,
-            },
-            {
-                source: '/technologies',
-                destination: `${process.env.BACKEND_URL}/technologies/`,
-            },
-            {
-                source: '/contact',
-                destination: `${process.env.BACKEND_URL}/contact/`,
-            },
-            {
-                source: '/hero',
-                destination: `${process.env.BACKEND_URL}/hero/`,
-            },
-            {
-                source: '/footer',
-                destination: `${process.env.BACKEND_URL}/footer/`,
-            },
-        ];
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: `${process.env.BACKEND_URL}/about/`,
+      },
+      {
+        source: '/projects',
+        destination: `${process.env.BACKEND_URL}/projects/`,
+      },
+      {
+        source: '/technologies',
+        destination: `${process.env.BACKEND_URL}/technologies/`,
+      },
+      {
+        source: '/contact',
+        destination: `${process.env.BACKEND_URL}/contact/`,
+      },
+      {
+        source: '/hero',
+        destination: `${process.env.BACKEND_URL}/hero/`,
+      },
+      {
+        source: '/footer',
+        destination: `${process.env.BACKEND_URL}/footer/`,
+      },
+    ];
+  },
 };
