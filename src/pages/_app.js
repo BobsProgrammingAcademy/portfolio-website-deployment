3b23eb3247748d71bb64cc01a8ddcfde6950cb1f
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-10-09 20:48:17 +0200

    Code refactoring

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,7 @@ import { CacheProvider } from '@emotion/react';
 
 import 'aos/dist/aos.css';
 
+// Components
 import { theme as customTheme } from '../theme';
 import createEmotionCache from '../utils/createEmotionCache';
 import ColorModeContext from '../components/ColorModeContext';
